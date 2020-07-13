0f404eb94aca446546f88896ed034a8119147a75
Lena Porina <porinalena@gmail.com>
2020-07-13 08:15:31 -0700

    remove handle nethod and replace it with a closure; add parseJSON method to decode data sent from the API

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Lena Porina on 7/13/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
